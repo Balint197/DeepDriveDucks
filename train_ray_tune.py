3af3b2c5d3104ea39c561a84b80e491454353f00
Hajnal Balint <hajnal.balint.david@gmail.com>
2021-12-12 18:47:21 +0100

    removed old unused files

--- train_ray_tune.py
+++
@@ -1,7 +0,0 @@
-# This file is for to train the baseline dagger algorithm with ray-tune hyper-parameter optimization.
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
-
-import notebook
